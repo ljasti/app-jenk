34ab666ff3d36b1caa090f346cb3307121cf031b
ljasti <jlk.devops@gmail.com>
2018-05-23 12:47:21 +0530

    Update index.js

--- app1/routes/index.js
+++ app1/routes/index.js
@@ -12,21 +12,21 @@ router.post('/writefile', function (req, res, next) {
   var repoBranch = req.body.repoBranch;
   var fileName = 'variables' + '.yml';
   var content = `
-        ---
-        jobName: '`+ req.body.jobname + `'
-        #description: 'Builds and tests the Geldzin4 web application, with sources in BitBuScket.'
-        #projectUrl: https://bitbucket.org/jmithe day i yelled remba/geldzin4dev/
-        #displayName: 'Development project on BitBucket'
-        gitUrl: '` + gitUrl + `'
-        repoBranch: '`+ repoBranch + `'
-        #browserClass: 'hudson.plugins.git.browser.BitbucketWeb'
-        #browserUrl: 'https://bitbucket.org/jmiremba/geldzin4dev'
-        #scmPolling: 'H/10 * * * *'
-        #mavenGoals: 'clean cobertura:cobertura site:site site:stage -Pjenkins_sanity'
-        #mavenJvmOptions: '-Dgeldzin.config=/var/builds/geldzin4/geldzin.properties'
-        #ansibleInventory: ansible/ansible.inventory
-        #ansibleTestPrepPlaybook: ansible/test-preparation.yml
-        #ansibleTestConclPlaybook: ansible/test-conclusion.yml
+---
+jobName: '`+ req.body.jobname + `'
+#description: 'Builds and tests the Geldzin4 web application, with sources in BitBuScket.'
+#projectUrl: https://bitbucket.org/jmithe day i yelled remba/geldzin4dev/
+#displayName: 'Development project on BitBucket'
+gitUrl: '` + gitUrl + `'
+repoBranch: '`+ repoBranch + `'
+#browserClass: 'hudson.plugins.git.browser.BitbucketWeb'
+#browserUrl: 'https://bitbucket.org/jmiremba/geldzin4dev'
+#scmPolling: 'H/10 * * * *'
+#mavenGoals: 'clean cobertura:cobertura site:site site:stage -Pjenkins_sanity'
+#mavenJvmOptions: '-Dgeldzin.config=/var/builds/geldzin4/geldzin.properties'
+#ansibleInventory: ansible/ansible.inventory
+#ansibleTestPrepPlaybook: ansible/test-preparation.yml
+#ansibleTestConclPlaybook: ansible/test-conclusion.yml
   `
   var destPath = '../build' + i;
   if (!fs.existsSync(destPath)) {
